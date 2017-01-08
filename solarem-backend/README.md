63de87a3f97522c2adb6749ffd79c2958330da10
Jyrki K <jyrkiokaski@gmail.com>
2017-01-08 15:28:43 +0000

    removed old trial

--- solarem-backend/README.md
+++
@@ -1,3 +0,0 @@
-# solarem-backend
-
-a [Sails](http://sailsjs.org) application
--- solarem-frontend/app/components/version/version.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('solaremFE.version', [
-  'solaremFE.version.interpolate-filter',
-  'solaremFE.version.version-directive'
-])
-
-.value('version', '0.1');
